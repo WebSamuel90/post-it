32be66dfbc528747ff7f0d8dc0d16ab6abfb15e4
Samuel <sumlebulle@hotmail.com>
2019-10-17 13:15:22 +0200

    Made firebase connections that needs to be corrected

--- post-it/src/components/Note.jsx
+++ post-it/src/components/Note.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { firestore } from '../firebase';
 
-const Note = ({ onRemove }) => {
+const Note = () => {
+
+    // const noteRef = firestore.doc(`notes/${id}`);
+    // const remove = () => noteRef.delete();
+    // const like = () => noteRef.update({ likes: like + 1 });
+
     return ( 
         <>
-            {/* <button className="delete" onClick={() => onRemove(id)}>X</button> */}
+            {/* <button className="delete" onClick={remove}>X</button>
+            <button className="like" onClick={like}>Like</button> */}
         </>
      );
 }
--- post-it/src/components/Notes.jsx
+++ post-it/src/components/Notes.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Note from './Note';
 // import AddNote from './AddNote';
 
-const Notes = ({ notes, onCreate, onRemove }) => {
+const Notes = ({ notes }) => {
     return ( 
         <>
-            {/* <AddNote onCreate={onCreate} />
-            {notes.map(note => <Note {...note} key={note.id} onRemove={onRemove} />)} */}
+            {/* <AddNote />
+            {notes.map(note => <Note {...note} key={note.id} />)} */}
         </>
     );
 }
