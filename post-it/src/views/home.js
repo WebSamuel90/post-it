38f24db7f8491a201b80b4f1b12d1be87dc154a0
RebeccaKlaening <klaeningrebecca@gmail.com>
2019-10-22 11:24:26 +0200

    postit can change color

--- post-it/src/views/home.js
+++ post-it/src/views/home.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 import Button from '../components/Button';
+import styled from 'styled-components';
+
+const WrapperStyled = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
 
 
 const Home = (props) => {
     return (
             <>
                 <h1>Home page</h1>
-                <Link to="/Host"><Button page="Host" /></Link>
-                <Link to="/User"><Button page="User" /></Link>
+                <WrapperStyled>
+                    <Link to="/Host"><Button name="Host" /></Link>
+                    <Link to="/User"><Button name="User" /></Link>
+                </WrapperStyled>
             </>
         
         );
