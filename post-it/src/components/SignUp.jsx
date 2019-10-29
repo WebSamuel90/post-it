93ba1310bac7af96072e9cb36a32ff46948c5a64
Samuel <sumlebulle@hotmail.com>
2019-10-29 09:01:50 +0100

    Deleted unused components. Commented back unused code for authentication

--- post-it/src/components/SignUp.jsx
+++ post-it/src/components/SignUp.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { auth } from 'firebase';
-import { createUserProfileDocument } from '../firebase';
+import { auth, createUserProfileDocument } from '../firebase';
 
 class SignUp extends Component {
     state = { displayName: '', email: '', password: '' };
